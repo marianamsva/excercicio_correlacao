0433228fd7ce780890c3042386ab0c4f209cd4b9
Mariana Monteiro <marianamsva@gmail.com>
2023-09-15 10:19:15 -0300

    Exercício

---
+++ excorrelacao.R
@@ -0,0 +1,18 @@
+## CORRELAÇÃO COM R ##
+# PRIMEIRO, VAMOS CARREGAR OS PACOTES
+pacman::p_load(corrplot, dplyr, ggplot2)
+
+# BASE DE DADOS IRIS SEM AS ESPÉCIES #
+iris2 <- iris %>% select(-Species)
+
+# TABELA DE CORRELAÇÃO COM TODAS AS VARIÁVEIS #
+cor(iris2)
+
+# GRÁFICOS DE DISPERSÃO PAREADOS DAS VARIÁVEIS #
+pairs(iris2)
+
+# CORRPLOT DAS VARIÁVEIS #
+irisCor <- cor(iris2) # Tabela de correlação
+corrplot(irisCor, method = "number", order = 'alphabet')
+corrplot(irisCor, order = 'alphabet') 
+corrplot(irisCor, method = "square", order = 'AOE')
